5066055f64dfaea3008c5f4c009a4b5768cbf45c
Pedro Henrique <pedrodguimaraes20@gmail.com>
2024-06-04 16:13:56 -0300

    User insertion

--- workshopmongo/src/main/java/com/example/workshopmongo/service/UserService.java
+++ workshopmongo/src/main/java/com/example/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.workshopmongo.domain.User;
+import com.example.workshopmongo.dto.UserDTO;
 import com.example.workshopmongo.repository.UserRepository;
 import com.example.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -28,5 +29,13 @@ public class UserService {
         }
         
         return user.get();
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO obj) {
+        return new User(obj.getId(), obj.getName(), obj.getEmail());
     }
 }
